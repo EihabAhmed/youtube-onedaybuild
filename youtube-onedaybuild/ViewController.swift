99c1b85b16c06f30283a19237dc464b33b6a2d08
Eihab Ahmed <eihabahmed@Eihabs-Mac.local>
2020-05-29 03:46:14 +0200

    initial Xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Eihab Ahmed on 5/29/20.
+//  Copyright © 2020 Eihab Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
